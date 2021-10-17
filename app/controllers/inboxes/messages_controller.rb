9885432f93f1c5af100e10edc57d8f3358527738
Yaro <yshmarov@gmail.com>
2021-10-17 20:27:30 +0000

    nested resources

--- app/controllers/inboxes/messages_controller.rb
+++ app/controllers/inboxes/messages_controller.rb
@@ -1,9 +1,11 @@
-class MessagesController < ApplicationController
+class Inboxes::MessagesController < ApplicationController
+  before_action :set_inbox
   before_action :set_message, only: %i[ show edit update destroy ]
 
   # GET /messages or /messages.json
   def index
-    @messages = Message.all
+    # @messages = Message.all
+    @messages = @inbox.messages
   end
 
   # GET /messages/1 or /messages/1.json
@@ -12,7 +14,8 @@ class MessagesController < ApplicationController
 
   # GET /messages/new
   def new
-    @message = Message.new
+    @message = @inbox.messages.build
+    # @message = Message.new
   end
 
   # GET /messages/1/edit
@@ -21,11 +24,12 @@ class MessagesController < ApplicationController
 
   # POST /messages or /messages.json
   def create
-    @message = Message.new(message_params)
+    # @message = Message.new(message_params)
+    @message = @inbox.messages.build(message_params)
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to @message, notice: "Message was successfully created." }
+        format.html { redirect_to [@inbox, @message], notice: "Message was successfully created." }
         format.json { render :show, status: :created, location: @message }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +42,7 @@ class MessagesController < ApplicationController
   def update
     respond_to do |format|
       if @message.update(message_params)
-        format.html { redirect_to @message, notice: "Message was successfully updated." }
+        format.html { redirect_to [@inbox, @message], notice: "Message was successfully updated." }
         format.json { render :show, status: :ok, location: @message }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,19 +55,23 @@ class MessagesController < ApplicationController
   def destroy
     @message.destroy
     respond_to do |format|
-      format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
+      format.html { redirect_to [@inbox, :messages], notice: "Message was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
+    def set_inbox
+      @inbox = Inbox.find(params[:inbox_id])
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_message
-      @message = Message.find(params[:id])
+      # @message = Message.find(params[:id])
+      @message = @inbox.messages.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def message_params
-      params.require(:message).permit(:title, :inbox_id)
+      params.require(:message).permit(:title)
     end
 end
--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,2 +1,4 @@
 class Inbox < ApplicationRecord
+  validates :title, presence: true
+  has_many :messages, dependent: :destroy
 end
